bb989ef5699439480554f36821183867a92e3c64
David Hay <misterhay@gmail.com>
2019-12-23 22:45:09 -0700

    Camera simulator working

---
+++ camera_simulator.py
@@ -0,0 +1,20 @@
+import socket
+from time import sleep
+
+port = 52381
+buffer_size = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', port))
+
+acknowledge_message = bytearray.fromhex('90 4Y FF'.replace('Y', '1')) # 1 = socket number
+completion_message = bytearray.fromhex('90 5Y FF'.replace('Y', '1'))
+
+while True:
+    data = s.recvfrom(buffer_size)
+    message = data[0]
+    address_port = data[1]
+    print(address_port, 'sent', message)
+    s.sendto(acknowledge_message, address_port)
+    sleep(0.1)
+    s.sendto(completion_message, address_port)
--- socket_send.py
+++ socket_send.py
@@ -1,9 +1,14 @@
 import socket
 
-socket_ip = '127.0.0.1'
-socket_port = 52381
+#camera_ip = '192.168.0.100'
+camera_ip = '127.0.0.1'
+camera_port = 52381
+buffer_size = 1024
 
-sending_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
+
+# only necessary for receiving, we can probably bind to camera_ip
+s.bind(('', camera_port+1)) # use the port one higher than the camera's port
 
 camera_on = '81 01 04 00 02 FF'
 sequence_number = 1
@@ -12,5 +17,15 @@ payload = bytearray.fromhex(camera_on)
 payload_length = len(payload).to_bytes(2, 'big')
 visca_message = payload_type + payload_length + sequence_number.to_bytes(4, 'big') + payload
 
-sending_socket.sendto(visca_message, (socket_ip, socket_port))
-print('Sent', camera_on, 'to', socket_ip)
+s.sendto(visca_message, (camera_ip, camera_port))
+print('Sent', camera_on, 'to', camera_ip)
+
+# listen for a response
+
+import binascii
+data = s.recvfrom(buffer_size)
+received_message = binascii.hexlify(data[0])
+print('Received', received_message)
+data = s.recvfrom(buffer_size)
+received_message = binascii.hexlify(data[0])
+print('Received', received_message)
