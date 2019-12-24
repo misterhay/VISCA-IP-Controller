a12bc3707439c9a5fed8122799b88531ba0208fb
David Hay <misterhay@gmail.com>
2019-12-23 23:46:38 -0700

    Getting closer

--- osc_to_visca.py
+++ osc_to_visca.py
@@ -10,11 +10,14 @@ import socket
 import binascii  # for printing the messages we send
 
 ### VISCA sender (socket)
-camera_ip = '192.168.0.100'
-#camera_ip = '127.0.0.1'
+#camera_ip = '192.168.0.100'
+camera_ip = '127.0.0.1'
 camera_port = 52381
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
 
+### VISCA receiver
+buffer_size = 1024
+s.bind(('', camera_port+1)) # use the port one higher than the camera's port
 
 ### VISCA Commands (Payloads)
 camera_on = '81 01 04 00 02 FF'
@@ -54,6 +57,7 @@ def reset_sequence_number():
     reset_sequence_number_message = bytearray.fromhex('02 00 00 01 00 00 00 01 01')
     s.sendto(reset_sequence_number_message,(camera_ip, camera_port))
     sequence_number = 1
+    print('Reset sequence number')
     return sequence_number
 
 ## Start off by resetting sequence number
@@ -67,23 +71,29 @@ def send_visca(message_string):
     payload_length = len(payload).to_bytes(2, 'big')
     visca_message = payload_type + payload_length + sequence_number.to_bytes(4, 'big') + payload
     s.sendto(visca_message, (camera_ip, camera_port))
-    print(binascii.hexlify(visca_message), camera_ip, camera_port, sequence_number)
+    print(binascii.hexlify(visca_message), 'sent to', camera_ip, camera_port, sequence_number)
     sequence_number += 1
     # wait for acknowledge and completion messages...
-    # acknowledge = 90 4Y FF  # Y = socket number
-    # completion = 90 5Y FF  (or 90 5Y ... FF for inquiry commands)
+    data = s.recvfrom(buffer_size)
+    received_message = binascii.hexlify(data[0])
+    print('Received', received_message)
+    data = s.recvfrom(buffer_size)
+    received_message = binascii.hexlify(data[0])
+    print('Received', received_message)
     return visca_message
 
 
-### OSC sender
-touchOSC_ip = '10.0.0.32'
-touchOSC_port = 9000
+### OSC server and client
+osc_receive_port = 8000
+touchOSC_ip = '10.0.0.32' # there must be a way to listen for this... osc_address[0]
+osc_send_port = 9000
 
-def sendOSC(osc_command, osc_argument):
+
+def send_osc(osc_command, osc_argument):
     print('this is not working...')
     #print(pan_speed)
     #send_loop = asyncio.get_event_loop()
-    #send_loop.run_until_complete(aiosc.send((touchOSC_ip, touchOSC_port), '/1/'+osc_command, osc_argument))
+    #send_loop.run_until_complete(aiosc.send((touchOSC_ip, osc_send_port), '/1/'+osc_command, osc_argument))
 
 ### OSC receiving server
 
@@ -95,19 +105,19 @@ def parse_osc_message(osc_address, osc_path, args):
     osc_command = osc_path_list[2]
     osc_argument = args[0]
     if osc_command == 'camera_on':
-        if osc_argument > 0:
-            send_visca(camera_on)
+        send_visca(camera_on)
     elif 'memory_' in osc_command:
         memory_preset_number = osc_command[-1]
         if osc_argument > 0:
             if 'recall' in osc_command:
+                print('Memory recall', memory_preset_number)
                 send_visca(information_display_off)
                 #wait for acknowledgement
                 send_visca(memory_recall.replace('p', memory_preset_number))
     elif osc_command == 'pan_tilt_speed':
         global pan_speed
         pan_speed = floor(osc_argument)
-        sendOSC('pan_tilt_speed_label', pan_speed)
+        send_osc('pan_tilt_speed_label', pan_speed)
     else:
         print(osc_command, osc_argument)
 
@@ -116,7 +126,7 @@ def protocol_factory():
     osc = aiosc.OSCProtocol({'//*': lambda osc_address, osc_path, *args: parse_osc_message(osc_address, osc_path, args)})
     return osc
 receive_loop = asyncio.get_event_loop()
-coro = receive_loop.create_datagram_endpoint(protocol_factory, local_addr=('0.0.0.0', 8000))
+coro = receive_loop.create_datagram_endpoint(protocol_factory, local_addr=('0.0.0.0', osc_receive_port))
 transport, protocol = receive_loop.run_until_complete(coro)
 receive_loop.run_forever()
 
