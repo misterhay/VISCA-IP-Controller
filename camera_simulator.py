775af91ce1ab5220674a98445ce2007dde819ed5
David Hay <misterhay@gmail.com>
2019-12-23 23:09:19 -0700

    Add binascii for nice printing

--- camera_simulator.py
+++ camera_simulator.py
@@ -1,5 +1,6 @@
 import socket
 from time import sleep
+import binascii
 
 port = 52381
 buffer_size = 1024
@@ -14,7 +15,7 @@ while True:
     data = s.recvfrom(buffer_size)
     message = data[0]
     address_port = data[1]
-    print(address_port, 'sent', message)
+    print(address_port, 'sent', binascii.hexlify(message))
     s.sendto(acknowledge_message, address_port)
     sleep(0.1)
     s.sendto(completion_message, address_port)
