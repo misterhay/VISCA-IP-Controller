5d860b0684148be46be92b6e1972e9f251dec2fc
David Hay <misterhay@gmail.com>
2020-01-11 09:32:11 -0700

    Fix socket call

--- listen_to_camera.py
+++ listen_to_camera.py
@@ -4,7 +4,7 @@ import binascii
 #camera_ip = '192.168.0.100'
 camera_port = 52381
 buffer_size = 1024
-
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', camera_port))
 
 visca_response_dictionary = {
