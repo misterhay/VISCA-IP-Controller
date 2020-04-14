b7e0f2f06a02aea513b8b80d73805f46a8a5c729
David Hay <misterhay@gmail.com>
2020-04-13 19:07:09 -0600

    Add command structure

--- stream_obs_recording_to_youtube.py
+++ stream_obs_recording_to_youtube.py
@@ -10,12 +10,20 @@ root = Tk()
 display_message = StringVar()
 root.title('Stream from OBS Recording to YouTube')
 Label(root, text='this is a label').grid(row=0, column=0, columnspan=100)
-streaming_key_entry = Entry(root)
-streaming_key_entry.grid(row=1, column=0)
-
-# streaming_key = streaming_key_entry.get()
-# ffmpeg_command = 
+stream_key_entry = Entry(root)
+stream_key_entry.grid(row=1, column=0)
 
 Button(root, text='Start Stream', command=lambda: start_streaming(ffmpeg_command)).grid(row=1, column=6)
 
 root.mainloop()
+
+# streaming_key = streaming_key_entry.get()
+# ffmpeg_command = 
+
+# ffmpeg -re -i /Users/davidhay/Movies/2020-04-13_18-56-01.mkv -acodec copy -vcodec copy -f flv rtmp://a.rtmp.youtube.com/live2/xxxx-xxxx-xxxx-xxxx
+
+recording_path = '/Users/davidhay/Movies/'
+filename = '2020-04-13_18-56-01.mkv'
+stream_key = 'xxxx-xxxx-xxxx-xxxx'
+ffmpeg_path = ''
+ffmpeg_command = ffmpeg_path+'ffmpeg -re -i '+recording_path+' -acodec copy -vcodec copy -f flv rtmp://a.rtmp.youtube.com/live2/'+stream_key
