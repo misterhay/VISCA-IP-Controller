1fff006acc7fe163b7485c415f7eca8edbd78e0c
David Hay <misterhay@gmail.com>
2021-04-18 11:16:27 -0600

    Add files

---
+++ class_testing.py
@@ -0,0 +1,12 @@
+class Camera:
+    def __init__(self, s=1, i=0):
+        sequence_number = s
+
+    def multiply(self, x, y):
+        print(x*y)
+        return x * y
+
+#print(camera.sequence_number)
+#print(Camera.multiply(4,5))
+c = Camera()
+c.multiply(3,4)
