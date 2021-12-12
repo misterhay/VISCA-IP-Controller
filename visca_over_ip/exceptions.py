0913c16ad9b2e7a22a1a25d3d7dc3b02ff1ad911
Andrew Blomenberg <andrewBlomen@gmail.com>
2021-12-12 11:14:14 -0700

    Added instance variables to exception class

--- visca_over_ip/exceptions.py
+++ visca_over_ip/exceptions.py
@@ -2,7 +2,7 @@ class ViscaException(RuntimeError):
     """Raised when the camera doesn't like a message that it received"""
 
     def __init__(self, response_body):
-        status_code = response_body[2]
+        self.status_code = response_body[2]
         descriptions = {
             1: 'Message length error',
             2: 'Syntax error',
@@ -11,5 +11,6 @@ class ViscaException(RuntimeError):
             5: 'No socket',
             0x41: 'Command not executable'
         }
+        self.description = descriptions[self.status_code]
 
-        super().__init__(f'Error when executing command: {descriptions[status_code]}')
+        super().__init__(f'Error when executing command: {self.description}')
