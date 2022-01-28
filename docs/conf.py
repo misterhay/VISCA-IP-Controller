d4907e1884c8cae33ee8616a910d83617452815a
Andrew Blomenberg <andrewBlomen@gmail.com>
2022-01-27 19:21:05 -0700

    Added copyright and added TOC to docs sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,8 @@ import visca_over_ip
 project = 'visca_over_ip'
 author = 'Yook74 and misterhay'
 version = visca_over_ip.__version__
+copyright = '2022, Andrew Blomenberg'
 
 extensions = ['sphinx.ext.autodoc']
 html_theme = 'nature'
+html_sidebars = {'**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html']}
