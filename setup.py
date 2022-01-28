d9a14543452d4ac84fdafa032379c59f234ccfa3
Andrew Blomenberg <andrewBlomen@gmail.com>
2022-01-27 19:37:30 -0700

    Updated setup.py metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
+import visca_over_ip
 
 with open('README.md', 'r') as handle:
     long_description = handle.read()
 
 setup(
     name='visca_over_ip',
-    version='0.1.0',
+    version=visca_over_ip.__version__,
     description='A driver package for the VISCA over IP protocol used by some Sony PTZ cameras',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Andrew Blomenberg',
+    author='Yook74 and misterhay',
     author_email='andrewBlomen@gmail.com',
     url='https://github.com/Yook74/VISCA-IP-Controller',
     packages=['visca_over_ip'],
